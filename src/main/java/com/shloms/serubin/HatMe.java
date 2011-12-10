fa06ff8a4a47a9384a48be7ac0242fbdd160dfe6
Serubin <serubin323@gmail.com>
2011-12-09 18:39:52 -0600

    Working on code for config.

--- src/main/java/com/shloms/serubin/HatMe.java
+++ src/main/java/com/shloms/serubin/HatMe.java
@@ -12,6 +12,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.material.*;
 //import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -20,38 +21,50 @@ public class HatMe extends JavaPlugin{
 
 	public static HatMe plugin;
 	public static Logger log = Logger.getLogger("Minecraft");
-	FileConfiguration config;
+	FileConfiguration config = getConfig();
+	
 	public static String version = "0.4.1";
 	public static String name = "HatMe";
+	
+	@Override
+	public void onDisable() {
+		//PluginDescriptionFile pdfFile =this.getDescription();
+		//this.logger.info(pdfFile.getName() + " is now disabled ");
+		log.info(name + "has been disabled");
+	}
+	@Override
+	public void onEnable(){
+		log.info(name + "version" + version + "has been enabled");
+		PluginManager pm = getServer().getPluginManager();
+		getConfig().options().copyDefaults(true);
+		
+
+		//this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " is enabled ");
+		}
+	
+	
+	
 	
 	  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		  Player player = (Player) sender;
 		  ItemStack itemHand = player.getItemInHand();
-		  if (commandLabel.equalsIgnoreCase("hat") || commandLabel.equalsIgnoreCase("hatme") || commandLabel.equalsIgnoreCase("hm")){
-			if (checkPermissionBasic(player)) {
-				 if (player.getItemInHand().getTypeId() == 0) {
-			         player.sendMessage(ChatColor.RED + "Please pick a valid item!");
-			         return true;
-				  } else {
-			             PlayerInventory inventory = player.getInventory();
-						 //Gets item from head
-			             ItemStack itemHead = inventory.getHelmet();
-						 //Removes item from hand
-			             inventory.removeItem(itemHand);
-						 //Sets item from hand to head
-			             inventory.setHelmet(itemHand);
-						 //Checks for item in head
-			             if (itemHead.getTypeId() != 0) {
-						 //sets item in head to hand
-			                 inventory.setItemInHand(itemHead);
-			             player.sendMessage(ChatColor.YELLOW + "You now have a hat!");
+		  int itemHandId = itemHand.getTypeId();
+		  List<Integer> rbBlocks = getConfig().getList("allowed");
+		  boolean rbAllow = getConfig().getBoolean("enabled");
+		  
+;		 if (commandLabel.equalsIgnoreCase("hat") || commandLabel.equalsIgnoreCase("hatme") || commandLabel.equalsIgnoreCase("hm")){
+				if (checkPermissionBasic(player)) {
+					if(rbAllow = true){
+						for(int allowID: rbBlocks){
+						if(itemHandId != allowID){
+							player.sendMessage(ChatColor.RED + "Invalid item");
 							return true;
-						}
-					  } 
-						}
-			 return true;
-					  }
-					 
+						}else{hatOn(sender); return true;}
+					}
+					}else{hatOn(sender); return true;}
+				}
+			  }
+				
 					  
 		  if (commandLabel.equalsIgnoreCase("unhat") || commandLabel.equalsIgnoreCase("unhatme") || commandLabel.equalsIgnoreCase("unhm")){
 			  if(checkPermissionBasic(player)){
@@ -63,9 +76,13 @@ public class HatMe extends JavaPlugin{
                     PlayerInventory inventory = player.getInventory();
                     int empty = inventory.firstEmpty();
                     ItemStack itemHead = inventory.getHelmet();                        //Get item in helmet
+                    if(empty == -1){
+                    	player.sendMessage(ChatColor.RED + "You have no space to take of your hat!");
+                    }else{
                     inventory.setHelmet(null);                     // removes item from helmet
                     inventory.setItem(empty, itemHead);              //Sets item from helmet to first open slot
                     return true;
+                    }
 				  	}  
 		  	}
 		  }
@@ -83,20 +100,6 @@ public class HatMe extends JavaPlugin{
 	// }
 	    
 	 
-	@Override
-	public void onDisable() {
-		//PluginDescriptionFile pdfFile =this.getDescription();
-		//this.logger.info(pdfFile.getName() + " is now disabled ");
-		log.info(name + "has been disabled");
-	}
-	@Override
-	public void onEnable(){
-		log.info(name + "version" + version + "has been enabled");
-		PluginManager pm = getServer().getPluginManager();
-		
-
-		//this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " is enabled ");
-		}
 	public boolean hatOn(CommandSender sender){
 		Player player = (Player) sender;
 	 if (player.getItemInHand().getTypeId() == 0) {
